086fc2d997a9787773617d5542b70812118f2c0c
luansantos <luansantos.coder@gmail.com>
2025-04-07 08:01:27 -0300

    Initial commit with unnecessary files ignored

---
+++ Source/Aura/Private/AuraCharacterBase.cpp
@@ -0,0 +1,19 @@
+// Copyright Druid Mechanics
+
+
+#include "AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
